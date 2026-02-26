ac1eb4a9dd97f4a7c27b0e72ba2107d24a373b5f
Programmeerman <maurits.kiens@rsg-sneek.nl>
2026-02-26 19:00:17 +0100

    beta 3, started to create apple

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var jump_sound: AudioStreamPlayer2D = $jumpSound
+@onready var death_sound: AudioStreamPlayer2D = $deathSound
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
 
+var alive = true
+
 
 func _physics_process(delta: float) -> void:
+	if !alive:
+		return
 	
 	# Add animation.
 	if velocity.x > 1 or velocity.x < -1:
@@ -37,4 +42,8 @@ func _physics_process(delta: float) -> void:
 		animated_sprite_2d.flip_h = false
 	elif direction == -1.0:
 		animated_sprite_2d.flip_h = true
-	
+
+func die() -> void:
+	death_sound.play()
+	animated_sprite_2d.animation = "dying"
+	alive = false
